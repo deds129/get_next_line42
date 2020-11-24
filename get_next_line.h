3a6fc84fdfbb09aea26489c9d31a09444ea58920
Hellen Anisha <hanisha@et-c6.kzn.21-school.ru>
2020-11-24 14:46:07 +0300

    add header

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanisha <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/24 14:37:11 by hanisha           #+#    #+#             */
+/*   Updated: 2020/11/24 14:41:11 by hanisha          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistdlib.h>
+# include <stdlib.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
