38cd98aed2f7180b9d5d1554bd38b20776a4fdae
deds <superman2000129@gmail.com>
2020-11-29 21:21:16 +0300

    crude gnl without large buffer support

--- get_next_line.c
+++ get_next_line.c
@@ -24,7 +24,8 @@ int get_next_line(int fd, char **line)
 	char				*buff;
 	//для хранения результата чтения файла (read)
 	int					read_byte;
-	//статическая переменная под остаток (после \0), чтобы продолжать чтение
+	//статическая переменная под остаток (после \0) буффера, чтобы продолжать
+	// чтение, чтобы не перепрыгивать через остаток буффера
 	static char 		*reminder;
 	//переменная указывающая на \n в буффере строки
 	char *p_buff;
@@ -38,10 +39,13 @@ int get_next_line(int fd, char **line)
 	//выделить память маллоком под строку
 	if(!(buff = (char *)(malloc(sizeof(char) * BUFFER_SIZE +1)) ))
 		return (-1);
-
-	//для того чтобы в line не было мусора изначально
-	ft_bzero(*line,ft_strlen(*line));
 	flag = 1;
+	//для того чтобы в line не было мусора изначально
+	if (reminder)
+		*line = ft_strdup(reminder);
+	else
+		*line = ft_strdup("");
+
 	//читаем строки и записываем все в одну строку
 	while (flag && (read_byte = read(fd,buff,BUFFER_SIZE)))
 	{
@@ -51,7 +55,12 @@ int get_next_line(int fd, char **line)
 		{
 			*p_buff = '\0';
 			flag = 0;
-			ft_strdup();
+			//записываем прочитанный, но не записанный остаток буффера в стат
+			// .переменную
+			p_buff++;
+
+			//проблемы с размером буффера
+			reminder = ft_strdup(p_buff);
 			//return (1);
 		}
 		*line = ft_strjoin(*line,buff);
@@ -63,13 +72,20 @@ int main(void)
 {
 	int fd = open("text.txt",O_RDONLY);
 	char *line;
-	int i;
+	//int i;
+	/*
 	while ((i = get_next_line(fd, &line)))
 	{
 		printf("i = %d %s\n",i,line);
 		free(line);
 	}
-	printf("i = %d %s\n",i,line);
+	 */
+	get_next_line(fd, &line);
+	printf("%s\n",line);
+
+	get_next_line(fd, &line);
+	printf("Next line : %s\n",line);
+
 	free(line);
 	return (0);
 }
